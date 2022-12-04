ea257c4a41bad214a6d89f847c39ab8984ab2cae
Hollow <3509984102@qq.com>
2022-12-04 16:16:50 +0800

    perf:http 服务和 app 业务拆分+路由和控制器拆分

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-const Koa = require("koa");
+const { APP_PORT } = require('./config/config.default')
 
-const { APP_PORT } = require("./config/config.default");
-
-const userRouter = require('./router/user.route')
-
-const app = new Koa();
-
-app.use(userRouter.routes())
+const app = require('./app')
 
 app.listen(APP_PORT, () => {
-  console.log(`server is running on http://localhost:${APP_PORT}`);
-});
+  console.log(`server is running on http://localhost:${APP_PORT}`)
+})
