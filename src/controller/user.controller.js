c0184d05cf23c2246932d4e68aef79efd7a794ff
Hollow <3509984102@qq.com>
2022-12-04 19:37:38 +0800

    feat:用户注册【单功能】异常数据校验+异常处理

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-const { createUser } = require("../service/user.service");
+const { createUser, getUerInfo } = require('../service/user.service')
 
 class UserController {
   async register(ctx, next) {
@@ -8,8 +8,36 @@ class UserController {
     // 2. 操作数据库
     const res = await createUser(user_name, password);
     // console.log(res)
+    // 合法性
+    if (!user_name || !password) {
+      console.error("用户名或密码为空", ctx.request.body);
+      ctx.status = 400;
+      ctx.body = {
+        code: "10001",
+        message: "用户名或密码为空",
+        result: "",
+      };
+      return;
+    }
+    // 合理性
+    if (getUerInfo({ user_name })) {
+      ctx.status = 409;
+      ctx.body = {
+        code: "10002",
+        message: "用户已经存在",
+        result: "",
+      };
+      return;
+    }
     // 3. 返回结果
-    ctx.body = ctx.request.body;
+    ctx.body = {
+      code: 0,
+      message: "用户注册成功",
+      result: {
+        id: res.id,
+        user_name: res.user_name,
+      },
+    };
   }
 
   async login(ctx, next) {
