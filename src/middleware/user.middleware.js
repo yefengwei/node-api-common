593dab3f06303a8c72f6f6d117dad4ea5a74423c
Hollow <3509984102@qq.com>
2022-12-04 20:43:59 +0800

    perf：用户模块中间件健壮性增强

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -4,11 +4,11 @@ const { userFormateError, userAlreadyExited } = require("../constant/err.type");
 const userValidator = async (ctx, next) => {
   const { user_name, password } = ctx.request.body;
   // 合法性
-  if (!user_name || !password) {
-    console.error("用户名或密码为空", ctx.request.body);
-    ctx.app.emit("error", userFormateError, ctx);
-    return;
-  }
+    if (!user_name || !password) {
+      console.error("用户名或密码为空", ctx.request.body);
+      ctx.app.emit("error", userFormateError, ctx);
+      return;
+    }
 
   await next();
 };
@@ -16,8 +16,21 @@ const userValidator = async (ctx, next) => {
 const verifyUser = async (ctx, next) => {
   const { user_name } = ctx.request.body;
   // 合理性
-  if (await getUerInfo({ user_name })) {
-    ctx.app.emit("error", userAlreadyExited, ctx);
+  //   if (await getUerInfo({ user_name })) {
+  //     ctx.app.emit("error", userAlreadyExited, ctx);
+  //     return;
+  //   }
+  try {
+    const res = await getUerInfo({ user_name });
+
+    if (res) {
+      console.error("用户名已经存在", { user_name });
+      ctx.app.emit("error", userAlreadyExited, ctx);
+      return;
+    }
+  } catch (err) {
+    console.error("获取用户信息错误", err);
+    ctx.app.emit("error", userRegisterError, ctx);
     return;
   }
 
