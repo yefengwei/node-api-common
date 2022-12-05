9e4fc59abda57982dc8701498aed6c6763ad2522
Hollow <3509984102@qq.com>
2022-12-05 14:33:08 +0800

    feat:token颁发

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,10 @@
-const { createUser, getUerInfo } = require('../service/user.service')
+const jwt = require("jsonwebtoken");
+
+const { createUser, getUerInfo } = require("../service/user.service");
+
+const { userRegisterError } = require("../constant/err.type");
+
+const {JWT_SECRET} = require('../config/config.default')
 
 class UserController {
   async register(ctx, next) {
@@ -6,21 +12,43 @@ class UserController {
     // console.log(ctx.request.body)
     const { user_name, password } = ctx.request.body;
     // 2. 操作数据库
-    const res = await createUser(user_name, password);
-    // console.log(res)
-    // 3. 返回结果
-    ctx.body = {
-      code: 0,
-      message: "用户注册成功",
-      result: {
-        id: res.id,
-        user_name: res.user_name,
-      },
-    };
+    try {
+      const res = await createUser(user_name, password);
+      // console.log(res)
+      // 3. 返回结果
+      ctx.body = {
+        code: 0,
+        message: "用户注册成功",
+        result: {
+          id: res.id,
+          user_name: res.user_name,
+        },
+      };
+    } catch (err) {
+      console.log(err);
+      ctx.app.emit("error", userRegisterError, ctx);
+    }
   }
 
   async login(ctx, next) {
-    ctx.body = "登录成功";
+    const { user_name } = ctx.request.body;
+
+    // 1. 获取用户信息(在token的payload中, 记录id, user_name, is_admin)
+    try {
+      // 从返回结果对象中剔除password属性, 将剩下的属性放到res对象
+      const { password, ...res } = await getUerInfo({ user_name });
+
+      ctx.body = {
+        code: 0,
+        message: "用户登录成功",
+        result: {
+          //expiresIn:1d 一天后过期
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: "1d" }),
+        },
+      };
+    } catch (err) {
+      console.error("用户登录失败", err);
+    }
   }
 }
 
