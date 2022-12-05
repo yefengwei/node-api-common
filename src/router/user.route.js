8e79b6ec6cfc9f3bb4c2e9e3e952c3c6fbf627ed
Hollow <3509984102@qq.com>
2022-12-05 16:00:46 +0800

    feat:认证中间件封装

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,14 +1,27 @@
 const Router = require("koa-router");
 //中间件用以校验数据
-const {userValidator,verifyUser,crpytPassword,verifyLogin} = require('../middleware/user.middleware')
+const {
+  userValidator,
+  verifyUser,
+  crpytPassword,
+  verifyLogin,
+} = require("../middleware/user.middleware");
+const {auth} = require("../middleware/auth.middleware");
 const { register, login } = require("../controller/user.controller");
 
 const router = new Router({ prefix: "/users" });
 
 // 注册接口
-router.post("/register",userValidator,verifyUser,crpytPassword, register);
+router.post("/register", userValidator, verifyUser, crpytPassword, register);
 
 // 登录接口
-router.post('/login', userValidator, verifyLogin, login)
+router.post("/login", userValidator, verifyLogin, login);
+
+// 修改密码接口
+// 修改密码接口
+router.patch('/', auth, (ctx, next) => {
+    console.log(ctx.state.user)
+    ctx.body = '修改密码成功'
+  })
 
 module.exports = router;
