14b3b0ce38d193bbcfe81a751fa1dd65865e6cb1
Hollow <3509984102@qq.com>
2022-12-05 13:19:08 +0800

    feat:登录验证

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,15 +1,22 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const { getUerInfo } = require("../service/user.service");
-const { userFormateError, userAlreadyExited } = require("../constant/err.type");
+const {
+  userFormateError,
+  userAlreadyExited,
+  userRegisterError,
+  userDoesNotExist,
+  userLoginError,
+  invalidPassword,
+} = require('../constant/err.type')
 
 const userValidator = async (ctx, next) => {
   const { user_name, password } = ctx.request.body;
   // 合法性
-    if (!user_name || !password) {
-      console.error("用户名或密码为空", ctx.request.body);
-      ctx.app.emit("error", userFormateError, ctx);
-      return;
-    }
+  if (!user_name || !password) {
+    console.error("用户名或密码为空", ctx.request.body);
+    ctx.app.emit("error", userFormateError, ctx);
+    return;
+  }
 
   await next();
 };
@@ -39,19 +46,46 @@ const verifyUser = async (ctx, next) => {
 };
 
 const crpytPassword = async (ctx, next) => {
-  const { password } = ctx.request.body
+  const { password } = ctx.request.body;
 
-  const salt = bcrypt.genSaltSync(10)//加10次盐-同步方法
+  const salt = bcrypt.genSaltSync(10); //加10次盐-同步方法
   // hash保存的是 密文
-  const hash = bcrypt.hashSync(password, salt)
+  const hash = bcrypt.hashSync(password, salt);
 
-  ctx.request.body.password = hash
+  ctx.request.body.password = hash;
 
-  await next()
-}
+  await next();
+};
+
+const verifyLogin = async (ctx, next) => {
+  // 1. 判断用户是否存在(不存在:报错)
+  const { user_name, password } = ctx.request.body;
+
+  try {
+    const res = await getUerInfo({ user_name });
+
+    if (!res) {
+      console.error("用户名不存在", { user_name });
+      ctx.app.emit("error", userDoesNotExist, ctx);
+      return;
+    }
+
+    // 2. 密码是否匹配(不匹配: 报错)
+    if (!bcrypt.compareSync(password, res.password)) {
+      ctx.app.emit("error", invalidPassword, ctx);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    return ctx.app.emit("error", userLoginError, ctx);
+  }
+
+  await next();
+};
 
 module.exports = {
   userValidator,
   verifyUser,
-  crpytPassword
+  crpytPassword,
+  verifyLogin
 };
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,6 +1,6 @@
 const Router = require("koa-router");
 //中间件用以校验数据
-const {userValidator,verifyUser,crpytPassword} = require('../middleware/user.middleware')
+const {userValidator,verifyUser,crpytPassword,verifyLogin} = require('../middleware/user.middleware')
 const { register, login } = require("../controller/user.controller");
 
 const router = new Router({ prefix: "/users" });
@@ -9,6 +9,6 @@ const router = new Router({ prefix: "/users" });
 router.post("/register",userValidator,verifyUser,crpytPassword, register);
 
 // 登录接口
-router.post("/login", login);
+router.post('/login', userValidator, verifyLogin, login)
 
 module.exports = router;
