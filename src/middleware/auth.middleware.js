5e4bede517556cb2ab16521e715fb2d0b02eea9f
Hollow <3509984102@qq.com>
2022-12-05 16:27:36 +0800

    feat:用户密码修改

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,7 +7,7 @@ const { tokenExpiredError, invalidToken } = require("../constant/err.type");
 const auth = async (ctx, next) => {
   const { authorization } = ctx.request.header;
   const token = authorization.replace("Bearer ", "");
-  console.log(token);
+  //console.log(token);
 
   try {
     // user中包含了payload的信息(id, user_name, is_admin)
@@ -16,10 +16,10 @@ const auth = async (ctx, next) => {
   } catch (err) {
     switch (err.name) {
       case "TokenExpiredError":
-        console.error("token已过期", err);
+        //console.error("token已过期", err);
         return ctx.app.emit("error", tokenExpiredError, ctx);
       case "JsonWebTokenError":
-        console.error("无效的token", err);
+        //console.error("无效的token", err);
         return ctx.app.emit("error", invalidToken, ctx);
     }
   }
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -6,8 +6,8 @@ const {
   crpytPassword,
   verifyLogin,
 } = require("../middleware/user.middleware");
-const {auth} = require("../middleware/auth.middleware");
-const { register, login } = require("../controller/user.controller");
+const { auth } = require("../middleware/auth.middleware");
+const { register, login,changePassword } = require("../controller/user.controller");
 
 const router = new Router({ prefix: "/users" });
 
@@ -19,9 +19,6 @@ router.post("/login", userValidator, verifyLogin, login);
 
 // 修改密码接口
 // 修改密码接口
-router.patch('/', auth, (ctx, next) => {
-    console.log(ctx.state.user)
-    ctx.body = '修改密码成功'
-  })
+router.patch("/", auth, crpytPassword, changePassword);
 
 module.exports = router;
